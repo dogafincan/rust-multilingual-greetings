aec310c1bd51ba211eab8791495bea15168aa6cc
Doga Fincan <doga@icloud.com>
2022-08-28 18:14:07 +0200

    Add first version of library

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use fastrand;
+
+pub fn random_greeting() -> &'static str {
+    let greetings = ["안녕하세요", "こんにちは", "你好", "สวัสดีครับ", "Xin chào"];
+    let test = fastrand::usize(0..5);
+    greetings[test]
 }
 
 #[cfg(test)]
@@ -7,8 +11,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn it_returns_random_greeting() {
+        fastrand::seed(1);
+        let result = random_greeting();
+        assert_eq!(result, "Xin chào");
     }
 }
