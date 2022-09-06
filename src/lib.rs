b9e83246df9321a9b7ac37cf08803f20175f1d49
Doga Fincan <doga@icloud.com>
2022-09-06 15:16:34 +0200

    Add assertions

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 pub fn random_greeting() -> &'static str {
     let greetings = ["안녕하세요", "こんにちは", "你好", "สวัสดีครับ", "Xin chào"];
-    let random_index = fastrand::usize(0..5);
+    debug_assert!(greetings.len() > 0, "expected at least one greeting");
+
+    for greeting in greetings {
+        debug_assert!(greeting.len() > 0, "expected a non-empty string")
+    }
+
+    let random_index = fastrand::usize(0..greetings.len());
     greetings[random_index]
 }
 
